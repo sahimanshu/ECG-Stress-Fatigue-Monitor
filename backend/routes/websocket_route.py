075b83d9876a3e9b9b79d1ea716c9c6a34dd3603
Wasam2005 <wasammahtab2005@gmail.com>
2026-07-16 02:31:44 +0530

    Update websocket_route.py

--- backend/routes/websocket_route.py
+++ backend/routes/websocket_route.py
@@ -1,14 +1,30 @@
 from fastapi import APIRouter, WebSocket
-from websocket.websocket_handler import handle_websocket
+
+from websocket.websocket_handler import (
+    handle_frontend,
+    handle_esp32
+)
 
 router = APIRouter()
 
 
-@router.websocket("/ws")
-async def websocket_endpoint(
+# Frontend WebSocket
+@router.websocket("/ws/frontend")
+async def frontend_socket(
     websocket: WebSocket
 ):
 
-    await handle_websocket(
+    await handle_frontend(
+        websocket
+    )
+
+
+# ESP32 WebSocket
+@router.websocket("/ws/esp32")
+async def esp32_socket(
+    websocket: WebSocket
+):
+
+    await handle_esp32(
         websocket
     )
